3d406ee841762d1aaf803169d763ce1656b44439
ross-eastman-mayden <ross.eastman@mayden.co.uk>
2015-11-05 19:13:29 +0000

    works now dunnit - interface next

--- postAndRail.php
+++ postAndRail.php
@@ -1,22 +1,31 @@
 <?php
+$rail = 1;
+$post = $rail + 1;
 
-$post = 11;
-$rail = 10;
+$postWidth = 0.10;
+$railLength = 1.5;
 
-$postLength = 10;
-$railLength = 20;
+$fenceLength = 100;
 
 $x = 0;
 
-function fenceLengthCalculator($postLength, $railLength, $post, $rail, $x)
-{
+if ($rail >= 1) {
+    function fenceLengthCalculator($postWidth, $railLength, $post, $rail)
+    {
 
-    (($rail * $railLength) + ($post * $postLength));
-    return $x;
-};
+        return (($rail * $railLength) + ($post * $postWidth));
+    }
 
-
-
-var_dump(fenceLengthCalculator($rail, $railLength, $post, $postLength, $x));
+    ;
 
 
+    echo $fenceLength = fenceLengthCalculator($rail, $railLength, $post, $postWidth, $x);
+} elseif ($rail == 0) {
+    function railNumberCalculator($fenceLength, $postWidth, $railLength, $railNumber = 0)
+    {
+        return ($fenceLength - $postWidth) / ($postWidth + $railLength);
+    }
+
+    echo $railNumber = ceil(railNumberCalculator($fenceLength, $postWidth, $railLength));
+    echo $postNumber = $railNumber + 1;
+}
