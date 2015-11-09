0b6f48c30ac176e8d8b7a1233515e36eb50d83e5
Ross E <ross.eastman@mayden.co.uk>
2015-11-09 17:23:58 +0000

    new testing start

--- src/OOPPostAndRail.php
+++ src/OOPPostAndRail.php
@@ -1,45 +1,48 @@
 <?php
 $railNumber = 0;
-$post = $railNumber + 1;
+$postNumber = $railNumber + 1;
 
 $postWidth = 0.10;
 $railLength = 1.5;
 
 $fenceLength = 50;
 
-class Fence {
+class Fence
+{
     public $fenceLength;
     public $railNumber;
     public $postNumber;
+    public $postWidth = 0.10;
+    public $railLength = 1.5;
 
     function fenceLengthCalculator($postWidth, $railLength, $post, $rail)
     {
-        if($rail >= 1) {
+        if ($rail >= 1) {
             return (($rail * $railLength) + ($post * $postWidth));
         }
     }
-
-$maxFenceLength = fenceLengthCalculator($postWidth, $railLength, $post, $rail);
-echo $maxFenceLength;
-
-} else {
-
-    function railNumberCalculator($fenceLength, $postWidth, $railLength, $rail = 0)
-    {
-        return ($fenceLength - $postWidth) / ($postWidth + $railLength);
-    }
-
-    echo $maxRailNumber = ceil(railNumberCalculator($fenceLength, $postWidth, $railLength, $railNumber)) . "</br>";
-    echo $maxPostNumber = $maxRailNumber + 1  . "</br>";
 }
-
-
-class Post {
-    public $width = 0.1;
-}
-
-class Rail {
-    public $length = 1.5;
-}
+//$maxFenceLength = fenceLengthCalculator($postWidth, $railLength, $post, $rail);
+//echo $maxFenceLength;
+//
+//} else {
+//
+//    function railNumberCalculator($fenceLength, $postWidth, $railLength, $rail = 0)
+//    {
+//        return ($fenceLength - $postWidth) / ($postWidth + $railLength);
+//    }
+//
+//    echo $maxRailNumber = ceil(railNumberCalculator($fenceLength, $postWidth, $railLength, $railNumber)) . "</br>";
+//    echo $maxPostNumber = $maxRailNumber + 1  . "</br>";
+//}
+//
+//
+//class Post {
+//    public $width = 0.1;
+//}
+//
+//class Rail {
+//    public $length = 1.5;
+//}
 
 
